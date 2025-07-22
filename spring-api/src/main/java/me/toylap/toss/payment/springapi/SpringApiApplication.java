be8811638eb758d513394c9bf8c830e9a0b5f1db
choijjangl <116607491+choijjangl@users.noreply.github.com>
2025-07-22 12:58:19 +0900

    [초기세팅]

---
+++ spring-api/src/main/java/me/toylap/toss/payment/springapi/SpringApiApplication.java
@@ -0,0 +1,13 @@
+package me.toylap.toss.payment.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiApplication.class, args);
+    }
+
+}
